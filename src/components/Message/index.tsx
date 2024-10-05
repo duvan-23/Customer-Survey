a014b54dce3e3a196c25a980357087c21deb3afa
Duvan Mendivelso <duvan.mendivelso@pysltda.com>
2024-10-05 07:11:36 -0500

    Changes modal design

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -5,11 +5,11 @@ const Message = ()=>{
         openModal
       }=useFormContext();
     return (
-        <div className={`fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/60`}>
+        <div className={`fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/10`}>
             <div className="relative bg-gradient-to-r from-indigo-200 via-purple-300 to-pink-200 rounded-lg shadow-lg overflow-hidden w-full max-w-lg">
                 <div className="p-8 space-y-4 text-center">
                     <h3 className="text-3xl font-semibold italic text-gray-800 mb-2">
-                        Notification
+                        Dear Customer
                     </h3>
                     <p className="text-gray-700 text-base leading-relaxed">
                         {openModal.text}
